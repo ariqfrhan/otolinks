8f734593f306bddb8b81692a3148dc3130037c4a
ariqfrhan <muhammadariq3@gmail.com>
2023-11-27 23:48:32 +0700

    `add` : logout button and clear state

--- app/src/main/java/maf/mobile/finalprojectpapb/ProfileFragment.java
+++ app/src/main/java/maf/mobile/finalprojectpapb/ProfileFragment.java
@@ -1,5 +1,6 @@
 package maf.mobile.finalprojectpapb;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import maf.mobile.finalprojectpapb.activity.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,13 +63,42 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    FirebaseUser mUser;
-
+    FirebaseAuth mAuth;
+    private EditText etEmail;
+    private EditText etPhone;
+    private EditText etUsername;
+    private Button btSave;
+    private Button btLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = (EditText) view.findViewById(R.id.etProfileEmail);
+        etPhone = (EditText) view.findViewById(R.id.etProfilePhone);
+        etUsername = (EditText) view.findViewById(R.id.etProfileUsername);
+
+        btSave = (Button) view.findViewById(R.id.btSave);
+        btLogout = (Button) view.findViewById(R.id.btLogout);
+
+
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return view;
     }
 }
